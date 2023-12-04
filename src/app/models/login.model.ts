87648f7f9ccebefa032d8d4b35943532ee2fc250
maram128 <134567208+maram128@users.noreply.github.com>
2023-12-04 21:44:31 +0100

    first commit

--- src/app/models/login.model.ts
+++
@@ -1,9 +0,0 @@
-export class Login{
-    constructor(
-               public title:string,
-                public description:string,
-                public imageUrl:string,
-                public createdDate:Date,
-                public snaps:number,){
-}
-}
--- src/app/service/login.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Login } from '../models/login.model';
-@Injectable({
-providedIn: 'root'
-})
-
-    export class LoginService {
-        login: Login[] = []
-        }
